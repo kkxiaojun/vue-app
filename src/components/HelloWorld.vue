07821c4750fab82f641198d6e470d9007da16943
zhaoguojun <985531883@qq.com>
2019-03-28 16:59:56 +0800

    add vue-vw解决方案

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li>1hahahnizaizuosmne</li>
+      <li>2hahahnizaizuosmne</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+$color: #ccc;
+ul>li{
+  font-size: 18px;
+  width: 500px;
+  float: left;
+  background-color: $color;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
